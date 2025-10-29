f5b612cba31583e9c4309aa407a692928df1158d
Rayo0131 <motunrayo188@gmail.com>
2025-10-29 22:17:51 +0100

    login and register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
     const[password, setPassword] = useState('');
     const navigate = useNavigate();
 
@@ -25,6 +26,7 @@ const Register = () => {
       <h2>Register</h2>
       <form onSubmit={handleRegister}>
         <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
         <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
         <button type="submit">Register</button>
       </form>

4076633e2ea98877bf083f5d23bd0d312aee5d5d
Rayo0131 <motunrayo188@gmail.com>
2026-01-06 07:47:00 +0100

    3rd commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { use } from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const navigate= useNavigate();
+  const token=localStorage.getItem('access_token');
+
+  const handleLogout=()=>{
+    localStorage.removeItem('access_token');
+    navigate('/');
+  }
+
+  let navItem = []
+
+  if(token){
+    navItem.push(
+      <Link to="/" className='text-white mx-2' key="login">Login</Link>
+      <Link to="/register" className='text-white mx-2' key="register">Sign Up</Link>
+    ) 
+  }else{
+    navItem.push(
+      <Link onClick={handleLogout} className='text-white mx-2' key="logout">Logout</Link>
+      <Link to="/register" className='text-white mx-2' key="register">Sign Up</Link>
+    )
+  }
+
   return (
 <nav className=' nav'>
     <h3>Task Manager</h3>
--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -6,6 +6,11 @@ const Task = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
+  const [editingId, setEditingId] = useState(null);
+  const [editingTitle, setEditingTitle] = useState('');
+  const [editingDescription, setEditingDescription] = useState('');
+
+
 
 const token=localStorage.getItem('access_token');
 
@@ -69,6 +74,34 @@ if (!token) {
     }
   };
 
+  const startEditing = (task) => {
+    setEditingId(task.id);
+    setEditingTitle(task.title);
+    setEditingDescription(task.description);
+  };
+
+  const cancelEditing = () => {
+    setEditingId(null);
+    setEditingTitle('');
+    setEditingDescription('');
+  };
+
+  const saveEdits = async (taskId) => {
+    try {
+      await axios.patch(`http://127.0.0.1:8000/api/tasks/${taskId}/`, {
+        title: editingTitle,
+        description: editingDescription
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      cancelEditing();
+      fetchTasks();
+    } catch (error) {
+      console.error("Error saving edits:", error);
+    }
+  };
 
   const handleDeleteTask = async (taskId) => {
     try {
@@ -84,28 +117,62 @@ if (!token) {
   };
 
   return (
-    <div>
-      <h2 className='text-light'>Task Manager </h2>
-      <form onSubmit={handleAddTask} className='task-field'>
-        <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} required />
-        <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} required />
-        <button type="submit">Add Task</button>
-      </form>
+    <div className='container mt-5  d-flex justify-content-center'>
 
-      <ul>
-        {tasks.length === 0 ? ( <p className='text-light'>No tasks available.</p>
-        ) : (
-          tasks.map((task) => (
-            <li key={task.id}>
-              <div>
-                <input type="checkbox" checked={task.completed} onChange={() => ToggleComplete(task.id, task.completed)} />
-                <span>{task.title}</span>
-                <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
-              </div>
-            </li>
-          ))
-        )}
-      </ul>
+      <div className='card p-4 bg-dark text-light shadow' style={{width: "100%", maxWidth: "600px"}}>
+              <h2 className='text-center mb-4 fw-bold'>Task Manager </h2>
+
+
+        <form onSubmit={handleAddTask} className='mb-4' >
+
+          <div className='mb-3'>
+            <input type="text" placeholder="Task Title" value={title} onChange={(e) => setTitle(e.target.value)} required className='form-control'/>
+            </div>
+
+          <div className='mb-3'>
+            <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} required className='form-control' />
+          </div>
+          <button type="submit" className='btn w-100 bg-primary'>Add Task</button>
+        </form>
+  
+        <ul className='list-unstyled '>
+          {tasks.length === 0 ? ( <p className='text-center text-secondary'>No tasks available.</p>
+          ) : (
+            tasks.map((task) => (
+              <li key={task.id} className='p-3 mb-3 rounded d-flex justify-content-between align-items-center' style={{background: '#222'}} >
+
+                <>
+                  <input className="form-control mb-2" 
+                   value={editingTitle} onChange={(e) => setEditingTitle(e.target.value)} />
+
+
+                  <input className="form-control mb-2" 
+                   value={editingDescription} onChange={(e) => setEditingDescription(e.target.value)} />
+
+                   <div>
+                    <button onClick={() => saveEdit(task.id)} className='btn btn-warning btn-sm '>Save</button>
+
+                    <button onClick={cancelEditing} className='btn btn-warning btn-sm'>Cancel</button>
+
+                   </div>
+                
+                </>
+                <div className='d-flex align-items-center gap-2'>
+                  <input type="checkbox" checked={task.completed} onChange={() => ToggleComplete(task.id, task.completed)} />
+                  <span className={task.completed ? "text-decoration-line-through text-success" : ""}>
+                    {task.title}
+                    </span>
+
+                  <button onClick={() => startEditing(task)} className='btn btn-warning btn-sm'>Edit</button>
+
+                  <button onClick={() => handleDeleteTask(task.id)} className='btn btn-danger btn-sm'>Delete</button>
+                </div>
+                <hr />
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
     </div>
   )
 }
